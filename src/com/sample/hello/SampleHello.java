565848c76e28733e627866d3564c66da975d3e08
Naveen Raj Y <naveenrajy@yahoo.com>
2020-02-04 14:35:36 +0530

    second commit added for loop

--- src/com/sample/hello/SampleHello.java
+++ src/com/sample/hello/SampleHello.java
@@ -5,6 +5,11 @@ public class SampleHello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
+		
 	}
 
 }
