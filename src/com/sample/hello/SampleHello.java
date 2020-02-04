5757d29ef4e0b8e025b28ea00a629893cf306b06
Naveen Raj Y <naveenrajy@yahoo.com>
2020-02-04 14:19:02 +0530

    added helloworld message

---
+++ src/com/sample/hello/SampleHello.java
@@ -0,0 +1,10 @@
+package com.sample.hello;
+
+public class SampleHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
